86045157d7b35369d4fa75b63207e907bc40050c
Daniel Wood <daniel.a.wood.jr@gmail.com>
2016-04-16 16:42:47 -0600

    Fixed gaussian dist. function to calculate appropriately.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::f64;
 
 fn main() {
-  //let e = f64::consts::E;
-  let e = 1 as f64;
-  //let a: [i32; 4] = [0, 1, 2, 4];
-  let a = [1, 9, 21];
+  let e = f64::consts::E;
+  let pi: f64 = f64::consts::PI;
+  //let e = 1 as f64;
+  let a: [f64; 4] = [0.0, 0.2, 0.5, 1.0];
+  //let a = [1, 9, 21];
   for &n in &a {
-    let nn: i32 = (n as i32 * (1/2)) * -1;
-    let tip: f64 = ((nn) as f64).powi(2);
+    let nn: f64 = -1.0 * (n/2.0);
+    let tip: f64 = nn.powi(2);
     let top: f64 = e.powf(tip);
-    let middle: f64 = f64::consts::PI;
-    let bottom: f64 = 2 as f64 * (middle);
+    let bottom: f64 = 2.0 * pi;
     let result: f64 = top.powf(tip)/bottom;
 
     println!("Gaussed {}: {:?}", n, result);
